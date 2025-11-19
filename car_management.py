86996341acd4c8fb686fc764a571d233c9f3c520
JoseMMedranoJr <jose.m.medranojr+port@gmail.com>
2025-11-18 21:20:28 -0600

    draft one

--- car_management.py
+++ car_management.py
@@ -1,2 +1,18 @@
-class CarManager:
-    pass
+all_cars = []
+    def __init__(self, id, make, model, year, mileage):
+        self._id = id
+        self._make = make
+        self._model = model
+        self._year = year
+        self._mileage = mileage
+        self._services = None
+        self.add_car()
+    
+    def add_car(self):
+        Car.all_cars.append(self)
+
+    def __str__(self):
+        return f"This is a Chevy: {self._id}"
+
+car1 = Car(1234, "Chevy", "Camaro", 2020, 13000)
+print(car1.all_cars)
